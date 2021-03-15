fd8b367bd5a647b55a14ee0a68412298c70da824
Dario <dario.valastro@gmail.com>
2021-03-15 23:38:35 +0100

    com

--- src/components/molecules/Nav-bar/NavBar.tsx
+++ src/components/molecules/Nav-bar/NavBar.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
-//import DropdownMenu, { DropdownItem, DropdownItemGroup } from '@atlaskit/dropdown-menu';
+import DropdownMenu, {
+  DropdownItem,
+  DropdownItemGroup,
+} from "@atlaskit/dropdown-menu"
 import "./NavBar.scss"
 import Avatar from "@atlaskit/avatar"
 
@@ -9,11 +12,21 @@ export function NavBar() {
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Navbar.Brand href="#home">JETop Credentials</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
-        <Avatar
-          appearance="square"
-          size="medium"
-          src="https://hello.atlassian.net/secure/projectavatar?pid=30630"
-        />
+        <DropdownMenu
+          trigger={
+            <Avatar
+              appearance="square"
+              size="large"
+              src="https://hello.atlassian.net/secure/projectavatar?pid=30630"
+            />
+          }
+          triggerType="button"
+          boundariesElement="viewport"
+        >
+          <DropdownItemGroup>
+            <DropdownItem>Logout</DropdownItem>
+          </DropdownItemGroup>
+        </DropdownMenu>
       </Navbar.Collapse>
     </Navbar>
   )
--- src/components/templates/protected-area/CredentialsPage.tsx
+++ src/components/templates/protected-area/CredentialsPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { useServiceContext } from "../../../hooks/useServiceContext"
 import { ServiceList } from "../../molecules/service-list/ServiceList"
 import { NavBar } from "../../molecules/Nav-bar/NavBar"
-import Footer from "../../atoms/footer/Footer"
+//import Footer from "../../atoms/footer/Footer"
 import Service from "../../../models/Service"
 
 function CredentialsPage() {
